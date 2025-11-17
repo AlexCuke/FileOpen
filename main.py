6e90ad5a118d0ebfcfc49b63c338386fb0f2db97
AlexCuke <alexcuke@gmail.com>
2025-11-18 00:29:26 +0300

    Four

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pprint
+'''
 cook_book = {}
 # Задание 1
 with open('cook_book.txt', encoding='utf-8') as file:
@@ -46,3 +47,37 @@ dishes=['Омлет','Салат Оливье']
 person_count=2
 
 get_shop_list_by_dishes(dishes, person_count)
+'''
+# Задание 3 
+new_file=[]
+new_files=[['1.txt'],['2.txt'],['3.txt']]
+for new_file in new_files:
+
+    with open(new_file[0], encoding='utf-8') as file:
+        count_string=0
+        while True:
+            line = file.readline().strip()
+            new_file.append(line)
+            if not line:
+                break
+            count_string +=1  # счетчик строк
+        #print(count_string)
+        new_file.insert(1,count_string)
+    print(new_file)
+print()
+i=0
+for i in range (len(new_files)-1):
+    if new_files[i][1]>new_files[i+1][1]:
+            spis=new_files[i]
+            new_files[i]=new_files[i+1]
+            new_files[i+1]=spis
+
+pprint.pprint(new_files)
+
+for new_file in new_files:
+     new_file[1]=str(new_file[1])
+
+
+with open('new.txt', 'w', encoding='utf-8') as file:
+    for new_file in new_files:
+        file.writelines([line + '\n' for line in new_file[:]])
